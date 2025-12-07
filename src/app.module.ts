a6fad2324177f11763a0d79ed53b615248b1a44a
DaiVyDev <daivydev.official@gmail.com>
2025-12-07 11:51:33 +0700

    fix: changing nodejs version in vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-
+// changing node version of nodejs from 24x to 20 solves the 500 internal server error in vercel
 @Module({
   imports: [
     ConfigModule.forRoot(),
